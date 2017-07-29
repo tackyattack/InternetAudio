3ca72e16157759afad3999c7aa1490fce5ba94f5
Henry Bergin <tackyattack@gmail.com>
2017-07-28 19:31:22 -0500

    Initial Commit

---
+++ InternetAudio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InternetAudio
+//
+//  Created by HENRY BERGIN on 7/28/17.
+//  Copyright © 2017 Henry Bergin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
